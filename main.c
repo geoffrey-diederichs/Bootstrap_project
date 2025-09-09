f8120ccff86c7b925b4ebbef488deefd77ce10e3
takezo <takezo@takezo-HP.(none)>
2025-09-09 11:17:52 +0200

    test

---
+++ main.c
@@ -0,0 +1,15 @@
+#iunclude "pwd.h"
+
+int main(void)
+{
+    char buffer[1024];
+    if (my_open(secret.csv))
+    {
+        create_file();
+    }
+    while(1)
+    {
+        buffer = read_line(0) || get_next_line(0);
+        if()
+    }
+}
