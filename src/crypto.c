8c56c4b0f31d246fb2c13467a7fc70fd54aaf4ab
eRa <geoffrey@geoffrey>
2025-09-09 13:12:08 +0200

    cleaning up

--- src/crypto.c
+++ src/crypto.c
@@ -1,4 +1,7 @@
-int decypher(char *data, const char *key, int size) {   
+int decypher(char *data, const char *key, int size) {
+    return 0;
 }
 
-
+int cypher(char *data, const char *key, int size) {
+    return 0;
+}
