414e99fe5f3f8efc087bf098d6f1b87e915d1178
eRa <geoffrey@geoffrey>
2025-09-10 16:43:24 +0200

    O_APPEND

--- include/libc/fcntl.h
+++ include/libc/fcntl.h
@@ -5,6 +5,7 @@
 #define O_WRONLY  1
 #define O_RDWR    2
 #define O_CREAT   00000100
+#define O_APPEND  02000
 
 typedef unsigned int mode_t;
 int open(const char *pathname, int flags, mode_t mode);
